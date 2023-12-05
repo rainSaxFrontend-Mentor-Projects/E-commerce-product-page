4a61680fb9597a046564dfb8243d25e83ab8ef58
rainSax <40616002+rainSax@users.noreply.github.com>
2023-12-05 15:28:27 -0800

    functional slideshow advance and previous buttons

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const slideshowBgs = [
+    "url('./images/image-product-1.jpg')",
+    "url('./images/image-product-2.jpg')",
+    "url('./images/image-product-3.jpg')"
+]
+var slideshowIdx = 0;
+
+setSlideshowBg(slideshowIdx)
+
 document.querySelector(".hamburger-icon").addEventListener("click", function () {
     document.querySelector(".hamburger-menu").classList.add("visible");
 })
@@ -5,3 +14,28 @@ document.querySelector(".hamburger-icon").addEventListener("click", function () 
 document.querySelector(".hamburger-close").addEventListener("click", function () {
     document.querySelector(".hamburger-menu").classList.remove("visible");
 })
+
+document.querySelector(".next-image").addEventListener("click", slideshowNext)
+document.querySelector(".back-image").addEventListener("click", slideshowPrev)
+
+function slideshowNext() {
+    slideshowIdx++;
+    if (slideshowIdx > slideshowBgs.length) {
+        slideshowIdx = 0;
+    }
+    setSlideshowBg(slideshowIdx)
+}
+
+function slideshowPrev() {
+    slideshowIdx--;
+    if (slideshowIdx < 0) {
+        slideshowIdx = 3;
+    }
+    setSlideshowBg(slideshowIdx)
+}
+
+function setSlideshowBg(index) {
+    document.querySelector(".slideshow").style.backgroundImage = slideshowBgs[index];
+    document.querySelector(".slideshow").style.backgroundRepeat = "no-repeat";
+    document.querySelector(".slideshow").style.backgroundSize = "cover";
+}
